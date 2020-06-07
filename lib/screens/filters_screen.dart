4703e8db8ef51f1eb0f01958614400a82d0106ce
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-07 12:01:48 +0530

    adding links to the drawer

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'filters',
+        ),
+      ),
+      body: Center(
+        child: Text('filters'),
+      ),
+    );
+  }
+}
